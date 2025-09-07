7e551f25a1db960db4acf9ea44c4d1aa3c49b45f
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-07 17:11:25 -0400

    corrected typo

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodingChallenge02a</title>
-    <h1> Product Pricing Assistanct</h1>
+    <h1> Product Pricing Assistant</h1>
 </head>
 <body>
     <script>
