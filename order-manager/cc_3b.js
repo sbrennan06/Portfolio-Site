bc731d8b9d92932ea6cc2baf3b7f57ef06c4b330
Samantha Brennan <samanthabrennan@usf.edu>
2025-09-10 20:40:16 -0400

    moving files

--- order-manager/cc_3b.js
+++
@@ -1,8 +0,0 @@
-// Coding Challenge 3b
-let inventory = [
-{sku:"SKU-001",name:"TACOS", price:19.99,stock:5},
-{sku:"SKU-002",name:"QUESO", price:9.99,stock:5},
-{sku:"SKU-003",name:"GUACAMOLE", price:.99,stock:5},
-{sku:"SKU-004",name:"CHIMICHANGAS", price:26.99,stock:5},
-];
-
